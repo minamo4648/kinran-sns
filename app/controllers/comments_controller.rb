6c9ca0b4035d9bea3ff65056473a97c5e1e67e51
TakashiKawano <minamo.kawano@nifty.com>
2016-02-23 03:20:00 +0000

    inst radius

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     @comment = @comments.build(comment_params)   
     
     @notice = Notice.new
-    @notice.body = "「#{@tanka.body[0,15]}...」に新しくコメントがつきました"
+    @notice.body = "「#{shorten(@tanka.body, 15)}」に新しくコメントがつきました"
     @notice.link = "/tankas/#{@tanka.id}"
     @notice.save
     @users = User.where('id in (?)', @tanka.comments.group(:user_id).pluck(:user_id) | [ @tanka.user.id ])
