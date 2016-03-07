ec21c828ba436b3f6ceaf329d3692c407ad96a23
TakashiKawano <minamo.kawano@nifty.com>
2016-03-07 04:16:11 +0000

    minimam renga

--- app/controllers/rengas_controller.rb
+++ app/controllers/rengas_controller.rb
@@ -1,7 +1,34 @@
 class RengasController < ApplicationController
+
+before_action :authenticate_user!
     
     def new
         
+        @rengas = Renga.where(kogyo_id: Kogyo.last.id, picked: true).order(place: :asc)
+        @renga = Renga.new
+        
+    end
+
+  def create
+    
+    @renga = current_user.rengas.build(renga_params)
+    
+    if @renga.save
+        
+        if Kogyo.last.pick_type == 1
+            Kogyo.last.update(place: Kogyo.last.place + 1)
+        end
+      redirect_to root_path, notice: "投稿が完了しました"
+    else
+      redirect_to new_renga_path
+    end
+
+  end
+
+  private
+
+    def renga_params
+      params.require(:renga).permit(:body, :place, :user_id, :kogyo_id, :picked)
     end
     
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -62,4 +62,32 @@ module ApplicationHelper
     
     end
     
+    def wareki(mydate)
+    
+        x = "平成" + i_to_kan(heisei(mydate.year)) + "年" + i_to_kan(mydate.month) + "月" + i_to_kan(mydate.day) + "日"
+        
+        return x
+    
+    end
+    
+    def kogyo_fase
+    
+        if Kogyo.count == 0
+            return 1
+        elsif Kogyo.last.place >= 2 and Kogyo.last.place <= 18
+            return 2
+        else
+            return 1
+        end
+    
+    end
+    
+    def place_name(num)
+    
+        x = ["","発句","脇　","三　","四　","月　","折端","折立","二　","三　","四　","五　","六　","七　","八　","九　","十　","花　","挙句"][num]
+    
+        return x
+    
+    end
+    
 end
